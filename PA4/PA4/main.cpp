6647438af03677c76610040f412f962fac894ee1
Eli Peters <elp5@humboldt.edu>
2018-03-26 07:39:54 -0700

    Checkin

--- PA4/PA4/main.cpp
+++ PA4/PA4/main.cpp
@@ -7,8 +7,54 @@
 
 using namespace std;
 
+void traverse(node* thingy)
+{
+	if (thingy == nullptr)
+	{
+		cout << "NULLPTR" << endl;
+		return;
+	}
+	if (thingy->isAnimal())
+	{
+		cout << "Is it a " << thingy->getText() << "?" << endl;
+		string answer;
+		cin >> answer;
+		while (answer != "yes" && answer != "no")
+		{
+			cout << "Sorry, I didn't understand what you said. (yes/no)" << endl;
+		}
+		if (answer == "yes")
+		{
+			cout << "Huzzah!" << endl;
+		}
+		else
+		{
+			cout << "Boo!" << endl;
+		}
+	}
+	else
+	{
+		cout << thingy->getText() << endl;
+		string answer;
+		cin >> answer;
+		while (answer != "yes" && answer != "no")
+		{
+			cout << "Sorry, I didn't understand what you said. (yes/no)" << endl;
+		}
+		if (answer == "yes")
+		{
+			traverse(thingy->getYesChild());
+		}
+		else
+		{
+			traverse(thingy->getNoChild());
+		}
+	}
+}
+
 int main()
 {
+	// CODE FOR CHECKIN
 	node *root = new node{"Does it walk on four legs?"};
 	root->setNoChild(new node{ "Does it fly?" });
 	root->getNoChild()->setNoChild(new node{ "*centipede" });
@@ -17,8 +63,7 @@ int main()
 	root->getYesChild()->setYesChild(new node{ "*cat" });
 	root->getYesChild()->setNoChild(new node{ "*dog" });
 
-	
-	
+	traverse(root);
 
 	return 0;
 }
